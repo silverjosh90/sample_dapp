0f36488c68aa472c117346a31c1f09ba099ff17f
Josh Silverstein <silverjosh90@gmail.com>
2017-07-02 14:55:14 -0700

    contract is creted

--- index.js
+++ index.js
@@ -4,29 +4,41 @@ var contract = './contracts/simple_storage.sol';
 var solc = require('solc');
 var fs = require('fs');
 var dotenv = require('dotenv').load();
+var input = fs.readFileSync('contracts/simple_storage.sol').toString();
+
+web3.personal.unlockAccount('ADDRESS', 'PLACEPASSWORDHERE')
 
 
-console.log('It is working', web3.eth.blockNumber);
-
-web3.eth.defaultAccount = web3.eth.accounts[0]
+web3.eth.defaultAccount = web3.eth.coinbase;
 var account = web3.eth.defaultAccount
 var balanceWei = web3.eth.getBalance(account).toNumber();
 var balance = web3.fromWei(balanceWei, 'ether')
 console.log('I am the balance', balance);
 
 
-function getContract(filename) {
-  return new Promise(function(resolve,reject){
-    fs.readFile(filename, 'utf8', function(err,result){
-      resolve(result);
-    })
-  })
-}
+var output = solc.compile(input , 1);
 
-getContract(contract).then(function(result){
-  var compiled = solc.compile(result, 1);
-	metaData = JSON.parse(compiled.contracts[':SimpleStorage'].metadata)
-	console.log('THIS IS THE METADATA', metaData.output.abi);
+var abi = JSON.parse(output.contracts[':SimpleStorage'].interface)
+var bytecode = output.contracts[':SimpleStorage'].bytecode;
+
+var contract = web3.eth.contract(abi);
+
+var gasEstimate = web3.eth.estimateGas({data: '0x'+bytecode});
+
+console.log('HERE IS THE ESTIMATE', gasEstimate);
+var deployedContract = contract.new({
+	data: '0x' + bytecode,
+	from: web3.eth.coinbase,
+	gas: gasEstimate
+}, function(err, contract){
+	console.log('THIS IS THE ERR', err);
+	console.log('THIS IS THE result', contract);
+})
 
 
-});
+var transactionHash = deployedContract.transactionHash;
+var contractAddress = deployedContract.address;
+
+
+console.log('THIS IS THE TRANSACTIONHash', transactionHash);
+console.log('THIS IS THE contract address', contractAddress);
